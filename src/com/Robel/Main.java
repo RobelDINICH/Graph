c6a4d027889101393e36765da9ec3577cd17aa32
Robel Nani <robelabiti@gmail.com>
2020-11-19 21:57:07 -0600

    Kinda Works I guess

--- src/com/Robel/Main.java
+++ src/com/Robel/Main.java
@@ -1,5 +1,15 @@
+/*
+File or Class name: Main, Grapher
+Program author(s): Robel Tadele
+Course number and title: COSC2203, Data Structures Section 1
+Assignment number and name: Can it be done? Comp B.
+Submission date: Nov 19 2020
+ */
+
+
 package com.Robel;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -9,18 +19,15 @@ public class Main {
 
         Scanner reader = new Scanner(System.in);
 
-        int numNodes = 0, stages = 0, outEdges = 0, edgesInCounter = 0, stageColumn = 0;
+        int numNodes, stages = 0, actCount = 0, outEdges = 0, edgesInCounter = 0, stageColumn = 0;
         boolean feasiblity = false;
         int[][] adjacencyM;
-        int[] stageValue;
-        int[] edgesIn;
-        int[] topologicalArr;
-        
-        int[] earlyActivity;
-        int[] est;
-        int[] lateActivity;
-        int[] lst;
-        
+        int[] stageValue, edgesIn, topologicalArr;
+
+        //Array for edges going into a given Stage
+        int[] earlyActivity, est;
+        int[] lateActivity, lst;
+
 
         System.out.println("Welcome to the AOE Maker ");
         System.out.println("Enter values \n");
@@ -49,66 +56,78 @@ public class Main {
             for (int j = 0; j < outEdges; j++) {
                 stageColumn = reader.nextInt();
                 adjacencyM[i][stageColumn - 1] = reader.nextInt();
+
+
             }//End of edge loop
         }//End of Stage Loop
 
-        for (stageColumn = 0; stageColumn < numNodes ; stageColumn++) {
+        for (stageColumn = 0; stageColumn < numNodes; stageColumn++) {
             edgesInCounter = 0;
             int m = stageColumn;
 
             //If value in matrix != -1 increase edges
             for (int i = 0; i < numNodes; i++) {
-                if(!(adjacencyM[i][m] == -1))
+                if (adjacencyM[i][m] != -1) {
                     edgesInCounter++;
-
+                    actCount++;
+                }
+                edgesIn[m] = edgesInCounter;
             }
-            edgesIn[m] = edgesInCounter;
         }
 
-        Grapher graph = new Grapher(adjacencyM, edgesIn);
-        
-        topologicalArr = graph.topologicalOrder(stageValue);
-        
-        //Setting Late Stage and Early Stage
-        est = graph.EarlyStager(topologicalArr, stageValue);
-        lst = graph.LateStager(est[est.length-1], topologicalArr, stageValue);
+            Grapher graph = new Grapher(edgesIn, adjacencyM);
 
-        //Checking Graph Visibility
-        feasiblity = graph.feasibility(topologicalArr, stageValue);
+            // Grapher graph = new Grapher(adjacencyM, edgesIn);
 
-        earlyActivity = graph.earlyActivity(7,5,est);
-        lateActivity = graph.lateActivity(lst,5,7);
-        String critical = graph.criticalActivity(lateActivity, earlyActivity);
+            //Storing Topological Array
+            topologicalArr = graph.topologicalOrdering(stageValue);
 
-        //Console Displayer
+            //Setting Late Stage and Early Stage
+            est = graph.earlyStager(topologicalArr, stageValue);
+            lst = graph.lateStager(est[est.length - 1], topologicalArr, stageValue);
 
-        if(feasiblity)
-            System.out.println("The project is Feasible");
-        else{
-            System.out.println("The project is not feasible");
-            System.exit(0);
+            //Checking Graph Feasibility
+            feasiblity = graph.isFeasible();
+
+
+            /////////////
+            earlyActivity = graph.earlyActivities(actCount, stageValue.length, est);
+            lateActivity = graph.lateActivities(actCount, stageValue.length, lst);
+
+
+            ArrayList<Integer> critical = graph.criticalActivities(lateActivity, earlyActivity);
+            //////////////////////////
+
+            //Console Displayer
+
+            if (feasiblity)
+                System.out.println("\nThe project is Feasible");
+            else {
+                System.out.println("\nThe project is not feasible");
+                System.exit(0);
+            }
+
+            //Prints Topological Ordering
+            System.out.print("Ordering: ");
+            for (int j : topologicalArr) System.out.print(j + " ");
+
+            System.out.println("\n\nStage      Early         Late");
+            for (int i = 0; i < lst.length; i++) {
+                if (i + 1 == topologicalArr[i])
+                    System.out.println(i + 1 + "            " + est[topologicalArr[i] - 1] + "            " + lst[topologicalArr[i] - 1]);
+                else
+                    System.out.println(i + 1 + "            " + est[i] + "            " + lst[i]);
+            }
+
+            //Prints Total Project Tim
+            System.out.println("\nTotal Projection Time: " + est[est.length - 1] + "\n");
+            System.out.println("Activity    Early     Late");
+
+            for (int i = 0; i < earlyActivity.length; i++)
+                System.out.println(i + 1 + "            " + earlyActivity[i] + "         " + lateActivity[i]);
+
+            //Prints Critical Activities
+            System.out.println("\nCritical Activities: " + critical);
+
         }
-
-        System.out.print("Ordering: ");
-        for (int j : topologicalArr) System.out.print(j + " ");
-
-
-        System.out.println("\n\nStage     Early     Late");
-        for (int i = 0; i < lst.length; i++) {
-            if(i+1 == topologicalArr[i])
-                System.out.println(i+1 + "          " + est[topologicalArr[i]-1] + "          " + lst[topologicalArr[i]-1]);
-            else
-                System.out.println(i+1 + "          " + est[i] + "          " + lst[i]);
-        }
-
-        System.out.println("\nTotal Projection Time: " + est[est.length-1] + "\n");
-        System.out.println("Activity     Early     Late");
-
-        for (int i = 0; i < earlyActivity.length; i++)
-            System.out.println(i+1 + "            " + earlyActivity[i] + "          " + lateActivity[i]);
-
-
-        System.out.println("\nCritical Activities: " + critical);
-
     }
-}
